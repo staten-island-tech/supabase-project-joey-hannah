0769a61bf84a8b00fc9f49bf6444607bc5912e78
joeyshi07 <joeys19@nycstudents.net>
2025-04-02 13:53:52 -0400

    set up routers

--- supabase-project/src/router/index.js
+++ supabase-project/src/router/index.js
@@ -1,5 +1,9 @@
 import { createRouter, createWebHistory } from 'vue-router'
 import HomeView from '../views/HomeView.vue'
+import ProfileView from '../views/ProfileView.vue'
+import DiscoveryView from '@/views/DiscoveryView.vue'
+import PostView from '@/views/PostView.vue'
+import ReviewsView from '@/views/ReviewsView.vue'
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
@@ -10,12 +14,24 @@ const router = createRouter({
       component: HomeView,
     },
     {
-      path: '/about',
-      name: 'about',
-      // route level code-splitting
-      // this generates a separate chunk (About.[hash].js) for this route
-      // which is lazy-loaded when the route is visited.
-      component: () => import('../views/AboutView.vue'),
+      path: '/profile',
+      name: 'profile',
+      component: ProfileView,
+    },
+    {
+      path: '/discovery',
+      name: 'discovery',
+      component: DiscoveryView,
+    },
+    {
+      path: '/posts',
+      name: 'posts',
+      component: PostView,
+    },
+    {
+      path: '/reviews',
+      name: 'review',
+      component: ReviewsView,
     },
   ],
 })
