e4f2c95235a872d13774be1d1090de0c837af51c
joeyshi07 <joeys19@nycstudents.net>
2025-04-03 13:22:33 -0400

    finish routing

--- supabase-project/src/App.vue
+++ supabase-project/src/App.vue
@@ -12,7 +12,7 @@ import HelloWorld from './components/HelloWorld.vue'
 
       <nav>
         <RouterLink to="/">Home</RouterLink>
-        <RouterLink to="/about">About</RouterLink>
+        <RouterLink to="/reviews">Reviews</RouterLink>
         <RouterLink to="/discovery">Discovery</RouterLink>
         <RouterLink to="/posts">Posts</RouterLink>
         <RouterLink to="/profile">Profile</RouterLink>
