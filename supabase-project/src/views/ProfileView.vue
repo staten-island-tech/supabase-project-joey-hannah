36ba15c4e589b74365081da49a883d1b65134237
joeyshi07 <joeys19@nycstudents.net>
2025-04-11 13:36:19 -0400

    Update ProfileView.vue

--- supabase-project/src/views/ProfileView.vue
+++ supabase-project/src/views/ProfileView.vue
@@ -1,38 +1,28 @@
 <template>
   <div>
     <h1 v-if="profiles.length">Profiles:</h1>
-      <li v-for="profile in profiles" :key="profile.id">
-        <h2>{{ profile.username }}</h2>
-        <p>{{ profile.name }}</p>
-        <img :src="profile.profile_pic" alt="Profile Picture"/>
-      </li>
+    <li v-for="profile in profiles" :key="profile.id">
+      <h2>{{ profile.username }}</h2>
+      <p>{{ profile.name }}</p>
+      <img :src="profile.profile_pic" alt="Profile Picture" />
+    </li>
   </div>
 </template>
 
 <script>
-<<<<<<< HEAD
 import { ref, onMounted } from 'vue'
 import { supabase } from '../supabaseClient.js'
-=======
-import { ref, onMounted } from 'vue';
-import { supabase } from '../supabaseClient.js'; 
->>>>>>> hannahbranch
 
-const profiles = ref([]);
+const profiles = ref([])
 
 async function getProfiles() {
-<<<<<<< HEAD
   const { data } = await supabase.from('profiles').select()
   profiles.value = data
-=======
-  const { data } = await supabase.from('profiles').select(); 
-   profiles.value = data; 
->>>>>>> hannahbranch
 }
 
 onMounted(() => {
-  getProfiles();
-});
+  getProfiles()
+})
 </script>
 
 <style scoped>
