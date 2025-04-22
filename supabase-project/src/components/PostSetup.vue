0ab179f3a12fdc9aa0f9314ccb69bf344b0c4225
HAnnahc77 <HAnnahc77@nycstudents.net>
2025-04-22 14:00:01 -0400

    post form

--- supabase-project/src/components/PostSetup.vue
+++ supabase-project/src/components/PostSetup.vue
@@ -1,7 +1,32 @@
 <template>
-  <div></div>
+  <div>
+    <form @submit.prevent="createPost">
+      <label for="image_url">Link to Image</label>
+      <input type="text" v-model="image_url" />
+      
+      <label for="caption">Caption</label>
+      <input type="text" v-model="caption" />
+
+      <button type="submit">Create Post</button>
+    </form>
+  </div>
 </template>
 
-<script setup></script>
+<script setup>
+import { ref } from 'vue'
+import { supabase } from '../supabaseClient.js'
+
+const image_url = ref('')
+const caption = ref('')
+
+function createPost{
+  const { error } = await supabase
+  .from('posts')
+  .insert({ image_url: image_url.value, caption: caption.value })
+}
+
+
+
+</script>
 
 <style lang="scss" scoped></style>
